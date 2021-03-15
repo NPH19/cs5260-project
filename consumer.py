2f2064f8fbf463930e24589db4dba3f0a31455f1
nick <hudson.nick.p@gmail.com>
2021-03-15 21:24:51 +0000

    added a main function

--- consumer.py
+++ consumer.py
@@ -1,12 +1,32 @@
 import logging
 import boto3
+import json
+from time import sleep
 
+class consumer():
 
-def WidgetCreateRequest():
-    pass
+    def __init__(self, store_strategy, bucket_2):
+        self.store_strategy = store_strategy
+        self.bucket_2 = bucket_2
 
-def WidgetDeleteRequest():
-    pass
+    def WidgetCreateRequest():
+        pass
+    
+    def WidgetDeleteRequest():
+        pass
+    
+    def WidgetChangeRequest():
+        pass
+    
+    
+def main():
+    s3client = boto3.client("s3")
+    
+    widget_json = s3client.get_object(Bucket='usu-cs-5260-hud-requests', Key='1612306368338')
+    widget = json.loads(widget_json)
+    
+    
+    
 
-def WidgetChangeRequest():
-    pass
+if __name__ == "__main__":
+    main()
