7c54f5a37ca12a3378f52a0f28d393f0ad5915e9
nick <hudson.nick.p@gmail.com>
2021-03-16 03:34:21 +0000

    completed the create s3 object part of the method

--- consumer.py
+++ consumer.py
@@ -10,22 +10,22 @@ class consumerClass():
         self.store_strategy = store_strategy
         self.s3client = boto3.client("s3")
         self.request_bucket = request_bucket
+        self.store_name = store_name
 
-    def WidgetCreateRequest(self, request):
+    def WidgetCreateRequest(self, widget):
         if self.store_strategy == 's3':
-            # widget_response = self.s3client.get_object(Bucket=self.request_bucket, Key=key)
-            # widget_dict = widget_response['ResponseMetadata']
-            # # widget = json.loads(str(widget_dict))
-            # print(widget_dict['owner'])
-            print(type(request))
+            key = 'widgets/'+str(widget['owner'])+'/'+str(widget['widgetId'])
+            self.s3client.put_object(Bucket=self.store_name, Key=key, Body=str(widget))
+            logging.info('key: ',key,' added successfully')
         else:
+            logging.warn('create widget did not add to s3 or DynamoDB')
             print('no good')
     
     def WidgetDeleteRequest():
-        pass
+        logging.info("Delete Request Encountered")
     
     def WidgetChangeRequest():
-        pass
+        logging.info("Change Request Encountered")
     
     
 def main():
@@ -33,19 +33,39 @@ def main():
     if len(sys.argv) != 5:
         print("ERROR: args did not match expected (see below)")
         print('python <filename>.py <name of request bucket> <\"s3\" or \"DynamoDB\"> <name of s3 or DB> <max time to run (seconds)>')
+        #python consumer.py usu-cs5260-hud-requests s3 usu-cs5260-hud-web 1
         sys.exit()
-    request_bucket = sys.argv[1]
+    req_bucket = sys.argv[1]
     store_strategy = sys.argv[2]
     store_name = sys.argv[3]
-    time_to_run = sys.argv[4]
+    time_to_run = int(sys.argv[4])
     
-    consumer = consumerClass(store_strategy, store_name, request_bucket)
+    consumer = consumerClass(store_strategy, store_name, req_bucket)
+    s3resource = boto3.resource("s3")
+    request_bucket = s3resource.Bucket(req_bucket)
+    s3client = boto3.client("s3")
     
-    while time()-start <= time_to_run:
-        request_list = request_bucket.objects.all()
-            for request in request_list:
-                consumer.WidgetCreateRequest(request)
-
+    # while time()-start <= time_to_run:
+    request_list = request_bucket.objects.all()
+    size = sum(1 for _ in request_list)
+    if size > 0:
+        for request in request_list:
+            key = request.key
+            print(request.key)
+            widget_response = s3client.get_object(Bucket=req_bucket, Key=key)
+            widget_stream = widget_response['Body']
+            widget = json.load(widget_stream)
+            # s3client.delete_object(Bucket=req_bucket, Key=key)
+            
+            if widget['type'] == 'create':
+                consumer.WidgetCreateRequest(widget)
+            elif widget['type'] == 'delete':
+                consumer.WidgetDeleteRequest(widget)
+            elif widget['type'] == 'change':
+                consumer.WidgetChangeRequest(widget)
+            else:
+                logging.warning("Request was not handled!")
+        
     print("finished running for given time")
     sys.exit
     
